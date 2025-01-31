d717043f2d2535aebf89e274b0ce2582e9ed78c7
Coder_Manuel <sirmanuel4@gmail.com>
2025-01-31 23:50:32 +0300

    perf(android): update open intent logic

--- android/src/main/kotlin/manuel/plugin/file_opener/FileOpenerPlugin.kt
+++ android/src/main/kotlin/manuel/plugin/file_opener/FileOpenerPlugin.kt
@@ -30,14 +30,8 @@ class FileOpenerPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
-            "getPlatformVersion" -> {
-                Log.d("FileOpener", "GetPlatform Version Called")
-                return result.success("Android ${android.os.Build.VERSION.RELEASE}")
-            }
-            "openFile" -> {
-                Log.d("FileOpener", "Open File Called")
-                openFile(call, result)
-            }
+            "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            "openFile" -> openFile(call, result)
             else -> result.notImplemented()
         }
     }
@@ -48,10 +42,7 @@ class FileOpenerPlugin : FlutterPlugin, MethodCallHandler {
 
     private fun openFile(call: MethodCall, result: Result) {
         try {
-            Log.d("FileOpener", "Arguments: ${call.arguments}")
             val filePath = call.argument<String>("path")
-            Log.d("FileOpener", "Attempting to open file: $filePath")
-
             if (filePath == null) {
                 result.error("INVALID_PATH", "File path cannot be null", call.arguments)
                 return
@@ -65,40 +56,31 @@ class FileOpenerPlugin : FlutterPlugin, MethodCallHandler {
                 return
             }
 
-            val uri =
-                    try {
-                        FileProvider.getUriForFile(
-                                context,
-                                "${context.packageName}.fileprovider",
-                                file
-                        )
-                    } catch (e: IllegalArgumentException) {
-                        // If the specific file location is not in the configured paths
-                        Log.e("FileOpener", "FileProvider error: ${e.message}")
+            val uri = try {
+                FileProvider.getUriForFile(
+                    context, "${context.packageName}.fileprovider", file
+                )
+            } catch (e: IllegalArgumentException) {
+                // If the specific file location is not in the configured paths
+                Log.e("FileOpener", "FileProvider error: ${e.message}")
 
-                        // Copy the file to a known directory
-                        val destFile = File(context.cacheDir, file.name)
-                        file.copyTo(destFile, overwrite = true)
+                // Copy the file to a known directory
+                val destFile = File(context.cacheDir, file.name)
+                file.copyTo(destFile, overwrite = true)
 
-                        FileProvider.getUriForFile(
-                                context,
-                                "${context.packageName}.fileprovider",
-                                destFile
-                        )
-                    }
+                FileProvider.getUriForFile(
+                    context, "${context.packageName}.fileprovider", destFile
+                )
+            }
 
             // Get the file MIME type
             val mimeType = getMimeType(file)
 
             // Create the intent
-            val intent =
-                    Intent(Intent.ACTION_VIEW).apply {
-                        setDataAndType(uri, mimeType)
-                        flags =
-                                Intent.FLAG_GRANT_READ_URI_PERMISSION or
-                                        Intent.FLAG_ACTIVITY_NEW_TASK or
-                                        Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    }
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                setDataAndType(uri, mimeType)
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
 
             // Start the intent
             context.startActivity(intent)
