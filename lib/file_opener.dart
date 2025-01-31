c821f61644feb7a5f7ec0b7716b329525e5bb4e9
Coder_Manuel <sirmanuel4@gmail.com>
2025-01-31 21:50:56 +0300

    perf(pubspec): update dart sdk constraint

--- lib/file_opener.dart
+++ lib/file_opener.dart
@@ -1,11 +1,11 @@
 import 'file_opener_platform_interface.dart';
 
 class FileOpener {
-  Future<String?> getPlatformVersion() {
+  static Future<String?> getPlatformVersion() {
     return FileOpenerPlatform.instance.getPlatformVersion();
   }
 
-  Future<void> openFile(String path) {
+  static Future<void> openFile(String path) {
     return FileOpenerPlatform.instance.openFile(path);
   }
 }
